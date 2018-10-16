c6ef3828f2d405d0c4afe48cefe8d2e2edc36d4d
Cerebellum <sebastian.telgen@outlook.com>
2018-10-16 11:35:37 +0200

    reading in mnist training data and plotting done

--- 02 scripts/load data.R
+++ 02 scripts/load data.R
@@ -1,3 +1,34 @@
 library(data.table)
+library(tidyverse)
+library(spatstat)
 
-a <- fread()
+train <- fread(file = "01 data/train.csv")
+train <- read.csv(file = "01 data/train.csv", header=T)
+
+colnames(train)
+print(table(train$label))
+
+## Explore the data set ##
+plot_digits= function(pixeldata){
+  
+  pixelmatrix <- matrix(
+    sapply(pixeldata[1, c(2:785)], as.numeric), 
+    nrow=28, ncol=28)
+  
+  pixelmatrix <- apply(pixelmatrix, 1, rev)
+  
+  image(t(pixelmatrix),
+      axes = FALSE, col = grey(seq(0, 1, length = 256)))
+  
+  title(main=pixeldata$label)
+}
+
+par(mfrow=c(4,4))
+for (idx in c(1:16)){
+  plot_digits(train[idx, ])
+  }
+
+
+
+
+
