cc0bcb10e563cb9e89fd0369dfb6ec599eeb87c8
Cerebellum <sebastian.telgen@outlook.com>
2018-10-17 12:57:27 +0200

    xgboost and evaluation fully functional. accuracy=97.28%

--- 02 scripts/load data.R
+++ 02 scripts/load data.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(spatstat)
 library(randomforest)
 library(xgboost)
+library(caret)
 
 train <- fread(file = "01 data/train.csv")
 train$label <- as.factor(train$label)
@@ -36,7 +37,7 @@ for (idx in c(1:16)){
 rf <- randomForest::randomForest(label~., train)
 
 ## Try a minimum solution using xgboost  #####
-train <- fread(file = "01 data/train.csv") 
+train <- fread(file = "01 data/train.csv") # relaod file, to make sure all cols are numeric
 sample_ids <- sample(c(1:42000))[1:40000]
 
 train_train <- train[sample_ids, ]
@@ -45,13 +46,24 @@ train_test <- train[-sample_ids, ]
 train_mat <-as.matrix(train_train)
 train_mat_xgb <- xgb.DMatrix(train_mat[,2:785], label=train_mat[,1])
 
-## continue here: Next step is to add one-hot encoding
+## continue here
+xgb_params <- list(booster = "gbtree", objective = "multi:softprob", num_class = 10, eval_metric = "mlogloss")
+#xgb_result <- xgboost(train_mat_xgb, nrounds=20, params=xgb_params)
 
-xgp_params <- c()
-xgb_result <- xgboost(train_mat_xgb, nrounds=20)
+# Do cross-validated xgboost
+xgbcv <- xgb.cv(params = xgb_params, data = train_mat_xgb, nrounds = 100, nfold = 5, showsd = TRUE, stratified = TRUE, 
+                print_every_n = 2, early_stop_round = 20, maximize = FALSE, prediction = TRUE)
 
-test_matrix <- xgb.DMatrix(data = test_data, label = test_label)
+# retreive predictions from class probabilities
+xgb_train_preds <- data.frame(xgbcv$pred) %>% mutate(pred_digit = max.col(., ties.method = "last") -1) 
+xgb_train_preds$real_digit =   as.vector(train_train[[1]])          
 
+xgb_conf_mat <- table(real=xgb_train_preds$real_digit, pred=xgb_train_preds$pred_digit)
 
+xgb_conf_mat_2 <- caret::confusionMatrix(factor(xgb_train_preds$real_digit),
+                                  factor(xgb_train_preds$pred_digit),
+                                  mode = "everything")
 
+print(xgb_conf_mat_2)
 
+#save.image()
